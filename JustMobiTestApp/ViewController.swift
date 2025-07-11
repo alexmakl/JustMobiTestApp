b8ed7bb32a45da539a05e97d92a1a75cf5f8ae9e
Alexander <mclakov@yandex.ru>
2025-07-11 10:31:00 +0300

    Project created

---
+++ JustMobiTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JustMobiTestApp
+//
+//  Created by Alexander on 11.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
